57189c34dd3af74dd2bb1fb8ab0b4533c6ab7b32
ExtevaXT <96924741+ExtevaXT@users.noreply.github.com>
2023-12-15 00:00:31 +0500

    replaced claim mod

--- src/main/java/su/external/adventure/command/ClaimCommand.java
+++ src/main/java/su/external/adventure/command/ClaimCommand.java
@@ -3,13 +3,14 @@ package su.external.adventure.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
-import dev.ftb.mods.ftbchunks.data.FTBChunksAPI;
 import net.minecraft.Util;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.level.ServerPlayer;
 import su.external.adventure.helpers.CoinHelper;
+import xaero.pac.OpenPartiesAndClaims;
+import xaero.pac.client.api.OpenPACClientAPI;
 
 public class ClaimCommand {
     // No configs?
@@ -20,11 +21,16 @@ public class ClaimCommand {
     }
     private static int execute(CommandContext<CommandSourceStack> command){
         if(command.getSource().getEntity() instanceof ServerPlayer player){
-            double modifier = Math.pow(DEFAULT_MODIFIER, FTBChunksAPI.getManager().getData(player).getClaimedChunks().size());
+            // This guy really loves interfaces
+            OpenPACClientAPI api = OpenPartiesAndClaims.INSTANCE.getClientAPI();
+            double modifier = Math.pow(DEFAULT_MODIFIER, api.getClaimsManager().getPlayerInfo(player.getUUID()).getClaimCount());
             int price = (int)(DEFAULT_PRICE * modifier);
+
             // First check then claim
-            if(CoinHelper.countCoins(player) >= price && FTBChunksAPI.claimAsPlayer(player, player.getLevel().dimension(), player.chunkPosition(), false).isSuccess())
+            if(CoinHelper.countCoins(player) >= price && api.getClaimsManager().get(player.getLevel().dimension().getRegistryName(), player.chunkPosition()) == null){
                 CoinHelper.takeCoins(player, price);
+                api.getClaimsManager().requestClaim(player.chunkPosition().x, player.chunkPosition().z, false);
+            }
             else player.sendMessage(new TranslatableComponent("command.claim.fail", price), Util.NIL_UUID);
         }
         return Command.SINGLE_SUCCESS;
